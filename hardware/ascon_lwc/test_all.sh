c7679a0f7fbde80f71ffb4eeb9f6d78c485da038
Robert Primas <robert.primas@iaik.tugraz.at>
2022-08-05 15:57:04 +0200

    clean up

--- hardware/ascon_lwc/test_all.sh
+++ hardware/ascon_lwc/test_all.sh
@@ -11,20 +11,11 @@ declare -a arr=(
 "v6"
 )
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
-
+make clean
 for i in "${arr[@]}"
 do
 	echo "$i"
-	make clean
 	make $i
-	#res=$(python3 $i | grep "SIMULATION FINISHED")
-	#if [[ $res == *"PASS"* ]]; then
-	#  	printf "${GREEN}PASS!${NC}\n"
-	#else
-    #	printf "${RED}FAIL!${NC}\n"
-	#fi
+	make clean
 done
 
