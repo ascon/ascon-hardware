3c1032a8845dac74590eab6f72c9c3fed9f91935
Robert Primas <rprimas@student.tugraz.at>
2022-08-05 19:24:02 +0200

    test script

--- hardware/ascon_lwc/test_all.sh
+++ hardware/ascon_lwc/test_all.sh
@@ -11,11 +11,20 @@ declare -a arr=(
 "v6"
 )
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 make clean
 for i in "${arr[@]}"
 do
-	echo "$i"
-	make $i
+	echo "Running testbench for $i..."
+	res=$(make $i | grep "::")
+	if [[ $res == *"PASS"* ]]; then
+		printf "${GREEN}PASS!${NC}\n"
+	else
+    		printf "${RED}FAIL!${NC}\n"
+	fi
 	make clean
 done
 
